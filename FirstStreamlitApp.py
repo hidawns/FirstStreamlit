93e9b92ebba80534ec15fb8c9e64cd08f49be0f6
hidawns <134594505+hidawns@users.noreply.github.com>
2024-08-29 16:03:29 +0700

    Update FirstStreamlitApp.py

--- FirstStreamlitApp.py
+++ FirstStreamlitApp.py
@@ -1,12 +1,19 @@
 import streamlit as st
-import pandas as pd
 import joblib
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sklearn
-import xgboost
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
+from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split, cross_val_score, learning_curve, KFold, GridSearchCV
+from sklearn import metrics
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 model = joblib.load('XGB_model.pkl')
 
