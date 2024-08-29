15ed4b82100990f407801208cc97660f267571d6
hidawns <134594505+hidawns@users.noreply.github.com>
2024-08-29 16:00:33 +0700

    Update FirstStreamlitApp.py

--- FirstStreamlitApp.py
+++ FirstStreamlitApp.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+import xgboost
+from xgboost import XGBRegressor
 
 model = joblib.load('XGB_model.pkl')
 
