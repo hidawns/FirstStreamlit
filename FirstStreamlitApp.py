a65c8d4179f547175469d5c5a3c59c3cf789dddb
hidawns <134594505+hidawns@users.noreply.github.com>
2024-08-30 06:51:44 +0700

    Update FirstStreamlitApp.py

--- FirstStreamlitApp.py
+++ FirstStreamlitApp.py
@@ -9,28 +9,28 @@ import joblib
 model = joblib.load('XGB_model (1).pkl')
 
 # Function to preprocess user inputs
-def preprocess_input(experience, num_of_qualifications, rating, miscellaneous_info, profile, place):
+def preprocess_input(experience, rating, place, profile, miscellaneous_info, num_of_qualifications):
     # Create a DataFrame with user inputs
     input_df = pd.DataFrame({
         'Experience': [np.sqrt(experience)],
-        'Num_of_Qualifications': [num_of_qualifications],
         'Rating': [rating],
-        'Miscellaneous_Info': [1 if miscellaneous_info == 'Present' else 0],
-        'Profile': [profile],
         'Place': [place],
+        'Profile': [profile],
+        'Miscellaneous_Info': [1 if miscellaneous_info == 'Present' else 0],
+        'Num_of_Qualifications': [num_of_qualifications],
         'Fee_category': [0.0]
     })
 
     # Encode the Profile and Place columns
+    le_place = LabelEncoder()
+    input_df['Place'] = le_place.fit_transform(input_df['Place'])
+    
     le_profile = LabelEncoder()
     input_df['Profile'] = le_profile.fit_transform(input_df['Profile'])
 
-    le_place = LabelEncoder()
-    input_df['Place'] = le_place.fit_transform(input_df['Place'])
-
     # Scaling
     scaler = MinMaxScaler()
-    cols_to_scale = ['Experience', 'Num_of_Qualifications', 'Rating', 'Miscellaneous_Info', 'Profile', 'Place', 'Fee_category']
+    cols_to_scale = ['Experience', 'Rating', 'Place', 'Profile', 'Miscellaneous_Info', 'Num_of_Qualifications', 'Fee_category']
     input_df[cols_to_scale] = scaler.fit_transform(input_df[cols_to_scale])
 
     return input_df
@@ -41,23 +41,20 @@ def main():
 
     # Input fields for user to provide data for prediction
     experience = st.number_input('Years of Experience', min_value=0, max_value=66, step=1)
-    num_of_qualifications = st.number_input('Number of Qualifications', min_value=1, max_value=10, step=1)
     rating = st.number_input('Doctor Rating', min_value=1, max_value=100, step=1)
-    
-    miscellaneous_info = st.selectbox('Miscellaneous Info Existent', ['Not Present', 'Present'])
-    
-    profile = st.selectbox('Doctor Specialization', ['Ayurveda', 'Dentist', 'Dermatologist', 'ENT Specialist', 'General Medicine', 'Homeopath'])
-    
     place = st.selectbox('Place', ['Bangalore', 'Mumbai', 'Delhi', 'Hyderabad', 'Chennai', 'Coimbatore', 'Ernakulam', 'Thiruvananthapuram', 'Other'])
-
+    profile = st.selectbox('Doctor Specialization', ['Ayurveda', 'Dentist', 'Dermatologist', 'ENT Specialist', 'General Medicine', 'Homeopath'])
+    miscellaneous_info = st.selectbox('Miscellaneous Info Existent', ['Not Present', 'Present'])
+    num_of_qualifications = st.number_input('Number of Qualifications', min_value=1, max_value=10, step=1)
+    
     # Prediction button
     if st.button('Predict'):
-        input_data = preprocess_input(experience, num_of_qualifications, rating, miscellaneous_info, profile, place)
+        input_data = preprocess_input(experience, rating, place, profile, miscellaneous_info, num_of_qualifications)
         
         # Make prediction
         prediction = model.predict(input_data)
         
-        st.write(f"Predicted Doctor Salary: ₹{round(prediction[0], 2)}")
+        st.write(f"Predicted Doctor Salary: {round(prediction[0], 2)}")
 
 if __name__ == '__main__':
     main()
